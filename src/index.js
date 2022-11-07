204e5e7f228340719cebb83bd52ba37dc1ec9240
aboynejames <james@aboynejames.co.uk>
2022-11-07 20:00:04 +0000

    message logic class and test file

--- src/index.js
+++ src/index.js
@@ -1,1 +1,33 @@
-console.log('{{hop-message}}')
+'use strict'
+/**
+*  message flow and logic routing
+*
+*
+* @class HopMessages
+* @package    HopMessages
+* @copyright  Copyright (c) 2022 James Littlejohn
+* @license    http://www.gnu.org/licenses/old-licenses/gpl-3.0.html
+* @version    $Id$
+*/
+import util from 'util'
+import EventEmitter from 'events'
+
+class HopMessages extends EventEmitter {
+
+  constructor() {
+    super()
+    console.log('{{HOP-Messages}}')
+  }
+
+  /**
+  * Message in
+  * @method messageIn
+  *
+  */
+  messageIn = function (message) {
+    console.log('message')
+  }
+
+}
+
+export default HopMessages
---
+++ test/live-index.test.js
@@ -0,0 +1,9 @@
+import assert from 'assert'
+import HopMessages from '../src/index.js'
+
+describe('message HOP live', function () {
+  it('hello message logic', function () {
+    let bbAI = new HopMessages
+    // assert.equal(bbAI.hello, 'bb-AI--{{hello}}')
+  })
+})
